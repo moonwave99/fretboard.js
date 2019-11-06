e309c652834f354afa3e4b1be338696166e65a51
Diego Caponera <diego.caponera@gmail.com>
2019-11-06 17:40:33 +0100

    Draft tests.

---
+++ src/scales/utils.test.js
@@ -0,0 +1,25 @@
+import test from 'ava';
+import { findMode } from './utils';
+
+const modes= [{
+  name: 'ionian',
+  aliases: ['major'],
+  root: 'E2',
+  index: 1
+},
+{
+  name: 'dorian',
+  root: 'F#2',
+  index: 2
+}];
+
+test('findMode', t => {
+  const found = findMode({ modes, modeName: 'ionian' });
+  t.deepEqual(modes[0], found);
+});
+
+test('findMode with alias', t => {
+  const found = findMode({ modes, modeName: 'ionian' });
+  const aliased = findMode({ modes, modeName: 'major' });
+  t.is(found, aliased);
+});

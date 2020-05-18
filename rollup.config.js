129e35eddf53a424fcd6e5af5f283b0893b93e1c
Diego Caponera <diego.caponera@gmail.com>
2020-05-18 10:12:39 +0200

    Resolve deps in rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
 
@@ -11,6 +12,7 @@ export default [
 			sourcemap: true
 		},
 		plugins: [
+			resolve(),
 			typescript()
 		]
 	},

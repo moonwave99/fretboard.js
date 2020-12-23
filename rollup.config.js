5c874af1345757e2d9ba23decee4ad03ce735bf8
Diego Caponera <diego.caponera@gmail.com>
2020-12-23 15:21:17 +0100

    Add type declarations to package.

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,37 @@
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
 export default [
-	{
-		input: 'src/index.ts',
-		output: {
-			name: 'fretboard',
-			file: pkg.browser,
-			format: 'umd',
-			sourcemap: true
-		},
-		plugins: [
-			resolve(),
-			typescript()
-		]
-	},
-	{
-		input: 'src/index.ts',
-		plugins: [
-			typescript()
-		],
-		external: [
-			...Object.keys(pkg.dependencies || {}),
-			...Object.keys(pkg.peerDependencies || {})
-		],
-		output: [
-			{ file: pkg.main, format: 'cjs', sourcemap: true },
-			{ file: pkg.module, format: 'es', sourcemap: true }
-		]
-	}
+  {
+    input: 'src/index.ts',
+    output: {
+      name: 'fretboard',
+      file: pkg.browser,
+      format: 'umd',
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      typescript({
+        typescript: require('typescript'),
+      }),
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    plugins: [
+      typescript({
+        typescript: require('typescript'),
+      }),
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    output: [
+      { file: pkg.main, format: 'cjs', sourcemap: true },
+      { file: pkg.module, format: 'es', sourcemap: true },
+    ],
+  },
 ];
